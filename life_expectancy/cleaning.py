b459525aff0cf8da08c61e26cbdcbe5705bc3c6e
Pedro Cardoso <pedro.a.cardoso@nos.pt>
2023-02-09 15:34:47 +0000

    assignement 3 init (not working)

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,13 +1,13 @@
 import argparse
 import pandas as pd
 
-def load_data():
+def load_data(path):
     """
     Function to load the raw data from eu life expectancy
 
     :return df: Returns pandas df with the raw data
     """
-    df=pd.read_csv('./life_expectancy/data/eu_life_expectancy_raw.tsv', sep='\t|,',engine ='python')
+    df=pd.read_csv(path, sep='\t|,',engine ='python')
     return df
 
 def clean_data(df):
@@ -34,23 +34,26 @@ def save_data(df,region):
     """
     df_filter_by_region=df[df.region==region]
     df_filter_by_region.to_csv('./life_expectancy/data/pt_life_expectancy.csv',index=False)
+    return df_filter_by_region.reset_index(drop=True)
 
-def main(region='PT'):
+def main(region='PT',path='./life_expectancy/data/eu_life_expectancy_raw.tsv'):
     """
     Calls all the functions to load, clean and save the data from life expectancy
 
     :param region: Optional region string parameter that defaults into 'PT'
     """
     # pylint: disable=redefined-outer-name
-    raw_df=load_data()
+    raw_df=load_data(path)
     clean_df=clean_data(raw_df)
-    save_data(clean_df,region)
+    clean_df_region=save_data(clean_df,region)
+    return clean_df_region
 
 if __name__ == "__main__":  # pragma: no cover
-    parser = argparse.ArgumentParser(description='Region input processing.')
+    parser = argparse.ArgumentParser(description='Input processing.')
+    parser.add_argument('path', metavar='path', \
+            type=str, nargs='?',help='path to input file',default='./life_expectancy/data/eu_life_expectancy_raw.tsv') 
     parser.add_argument('region', metavar='region', \
-            type=str, nargs='?',help='region string (example for Portugal: \'PT\')',default='PT')
+            type=str, nargs='?',help='region string (example for Portugal: \'PT\')',default='PT')      
     args = parser.parse_args()
-    raw_df=load_data()
-    clean_df=clean_data(raw_df)
-    save_data(clean_df,args.region)
+    main(region=args.region,path=args.path)
+
--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,15 +1,27 @@
 """Tests for the cleaning module"""
 import pandas as pd
-
-from life_expectancy.cleaning import main
+from life_expectancy.cleaning import main,save_data
 from . import OUTPUT_DIR
+import life_expectancy.cleaning  
+import unittest
+from unittest.mock import patch 
 
-def test_clean_data(pt_life_expectancy_expected):
+     
+@patch('life_expectancy.cleaning.pd.DataFrame.to_csv',return_value=None)
+def test_save(mock):
+    mock.side_effect = print(
+      "Output not saved to file, because it is a test."    )
+    #self.assertTrue(life_expectancy.cleaning.pd.DataFrame.to_csv().called)
+
+
+
+def test_clean_data(pt_life_expectancy_expected,sample_data):
     """Run the `main` function from cleaning.py and compare the output to the expected output"""
-    main()
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+    df=main(path=sample_data)
+    # pt_life_expectancy_actual = pd.read_csv(
+    #     OUTPUT_DIR / "pt_life_expectancy.csv"
+    # )
+    print(pt_life_expectancy_expected)
     pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
+        df, pt_life_expectancy_expected
     )
